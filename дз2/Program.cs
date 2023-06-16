0d69335deefc6c3faca1d35e5979affa51aca2a5
яна <musina.yana02@yandex.ru>
2023-06-16 14:58:54 +0500

    вездедоабвила решения

--- дз2/Program.cs
+++ дз2/Program.cs
@@ -2,15 +2,21 @@
 //2, 3, 7 -> 7
 //44 5 78 -> 78
 //22 3 9 -> 22
-System.Console.WriteLine("Введите число: ");
+System.Console.WriteLine("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите число: ");
+System.Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Введите число: ");
+System.Console.WriteLine("Введите третье число: ");
 int number3 = Convert.ToInt32(Console.ReadLine());
-if (number1>number2 & number1>number3)
+if (number1>number2 && number1>number3)
+{
     System.Console.WriteLine("max:" + number1);
-    else if (number2>number1 & number2>number3)
-        System.Console.WriteLine("max:" + number2);
-        else if (number3>number1 & number3>number2);
-             System.Console.WriteLine("max:" + number3);
+}
+if (number2>number1 && number2>number3)
+{
+    System.Console.WriteLine("max:" + number2);
+}
+if (number3>number1 && number3>number2)
+{
+    System.Console.WriteLine("max:" + number3);
+}
---
+++ дз4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("Введите число : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Результат : ");
+for (int i =2 ; i <= number1; i=i+2)
+{
+ Console.Write(+ i + " ");
+}
+Console.WriteLine();
