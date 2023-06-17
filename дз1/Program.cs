a8c7c6e5b0fa8f383bf945824334085af4209cb3
яна <musina.yana02@yandex.ru>
2023-06-17 11:51:24 +0500

    Добавление гитигнор

--- дз1/Program.cs
+++ дз1/Program.cs
@@ -3,6 +3,7 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+
 System.Console.WriteLine("Введите число ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите число");
